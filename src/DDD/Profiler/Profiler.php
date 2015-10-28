85f9ef4346672c04e3b6aa221bb2198bffb0a3ce
ElSuperbeano <marius.schuette@gmail.com>
2015-10-28 05:27:09 +0100

    playing around

--- src/DDD/Profiler/Profiler.php
+++ src/DDD/Profiler/Profiler.php
@@ -9,6 +9,11 @@ abstract class Profiler
 
     abstract public function execute();
 
+    /**
+     * @param string $class
+     * @param string $method
+     * @param float $average
+     */
     protected function addMessage($class, $method, $average)
     {
         $this->messages[] = sprintf("%s - $average", str_replace(__CLASS__, $class, $method));
