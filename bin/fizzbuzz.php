da0ad930a7717e19e0332495fbe275dd558a653b
ElSuperbeano <marius.schuette@gmail.com>
2016-08-22 23:55:20 +0200

    playing around

--- bin/fizzbuzz.php
+++ bin/fizzbuzz.php
@@ -2,5 +2,5 @@
 <?php
 
 for ($i = 0; $i < 100; $i++) {
-    echo ($i % 3 == 0 ? 'Fizz' : '') . ($i % 5 == 0 ? 'Buzz' : '') . PHP_EOL;
+    echo (($i % 3 == 0 || $i % 5 == 0) ? ($i % 3 == 0 ? 'Fizz' : '') . ($i % 5 == 0 ? 'Buzz' : '') : $i) . PHP_EOL;
 }
