5c58a7a788b787447d203a441fcb03b942e2230a
Marius Schütte <mschuette@goodgamestudios.com>
2017-04-04 11:29:17 +0200

    playing around with JavaScript

--- public/js/101.js
+++ public/js/101.js
@@ -1,21 +1,21 @@
 var app = {
     init: function () {
-        $('#example').DataTable({
-            "dom": "Bfrtip",
-            "processing": true,
-            "serverSide": true,
-            "ajax": "data/api.php",
-            columns: [
-                { data: "name" },
-                { data: "position" },
-                { data: "office" },
-                { data: "extn" },
-                { data: "start_date" },
-                { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) }
-            ],
-            "buttons": [
-                "csv"
-            ]
+        $.get('data/buttons.php').done(function(buttons) {
+            $('#example').DataTable({
+                "dom": "Bfrtip",
+                "processing": true,
+                "serverSide": true,
+                "ajax": "data/api.php",
+                columns: [
+                    { "data": "name" },
+                    { "data": "position" },
+                    { "data": "office" },
+                    { "data": "extn" },
+                    { "data": "start_date" },
+                    { "data": "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) }
+                ],
+                "buttons": JSON.parse(buttons)
+            });
         });
     }
 };
