f6250766ebd7dda9e69dffa6d0e9b3ebbfdd2058
Marius Schütte <mschuette@goodgamestudios.com>
2019-01-04 09:57:33 +0100

    TableGateway

---
+++ src/TableGateway/DoctrineTableGateway.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace TableGateway;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\FetchMode;
+
+class DoctrineTableGateway
+{
+    /** @var Connection */
+    private $connection;
+    /** @var DataMap */
+    private $dataMap;
+
+    /**
+     * @param Connection $connection
+     * @param DataMap $dataMap
+     */
+    public function __construct(Connection $connection, DataMap $dataMap)
+    {
+        $this->connection = $connection;
+        $this->dataMap = $dataMap;
+    }
+
+
+    /**
+     * @param string $where
+     * @return array
+     */
+    public function all($where = '1')
+    {
+        $sql = $this->connection->createQueryBuilder();
+        $sql->select($this->dataMap->selectColumns())
+            ->from($this->dataMap->tableName())
+            ->where($where);
+
+        return $sql->execute()->fetchAll(FetchMode::ASSOCIATIVE);
+    }
+
+    public function byId(string $id): array
+    {
+        $sql = $this->connection->createQueryBuilder();
+        $pk = $this->dataMap->primaryKey();
+        $sql->select($this->dataMap->selectColumns())
+            ->from($this->dataMap->tableName())
+            ->where("{$pk}=:{$pk}")
+            ->setParameters([$pk => $id]);
+
+        return $sql->execute()->fetch(FetchMode::ASSOCIATIVE);
+    }
+
+    public function insert(array $params)
+    {
+        $params = array_filter(
+            $params,
+            function ($key) {
+                return in_array($key, $this->dataMap->insertColumns());
+            },
+            ARRAY_FILTER_USE_KEY
+        );
+        $values = array_reduce(array_keys($params), function (array $carry, $key) { $carry[$key] = ":{$key}"; }, []);
+        $sql = $this->connection->createQueryBuilder();
+        $sql->insert($this->dataMap->tableName())->values($values);
+        $sql->setParameters($params);
+        $sql->execute();
+    }
+
+    public function update(string $id, array $params)
+    {
+        $params = array_filter(
+            $params,
+            function ($key) {
+                return in_array($key, $this->dataMap->updateColumns());
+            },
+            ARRAY_FILTER_USE_KEY
+        );
+        $pk = $this->dataMap->primaryKey();
+        $sql = $this->connection->createQueryBuilder();
+        $sql->update($this->dataMap->tableName());
+        foreach ($params as $key => $value) {
+            $sql->set($key, ":{$key}");
+        }
+        $sql->where("{$pk}=:{$pk}");
+        $sql->setParameters(array_merge($params, [$pk => $id]));
+        $sql->execute();
+    }
+
+    public function delete(string $id)
+    {
+        $pk = $this->dataMap->primaryKey();
+        $sql = $this->connection->createQueryBuilder();
+        $sql->delete($this->dataMap->tableName())->where("{$pk}=:{$pk}")->setParameters([$pk => $id]);
+        $sql->execute();
+    }
+}
